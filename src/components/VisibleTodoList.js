c762c394883b8791ec979be6e825ec0b39293e6a
hari krisha <hari.k.krishna4@gmail.com>
2018-05-08 16:26:12 -0500

    updated with semicolons

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,37 +1,39 @@
-import { connect } from 'react-redux';
-import { toggleTodo } from '../actions';
+import {
+    connect,
+} from 'react-redux';
+import {
+    toggleTodo,
+} from '../actions';
 import TodoList from './TodoList';
 
 const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-    case 'SHOW_ALL':
-      return todos;
-    case 'SHOW_COMPLETED':
-      return todos.filter(t => t.completed);
-    case 'SHOW_ACTIVE':
-      return todos.filter(t => !t.completed);
-    default:
-      throw new Error(`Unknown filter: ${filter}.`);
-  }
+    switch (filter) {
+        case 'SHOW_ALL':
+            return todos;
+        case 'SHOW_COMPLETED':
+            return todos.filter(t => t.completed);
+        case 'SHOW_ACTIVE':
+            return todos.filter(t => !t.completed);
+        default:
+            throw new Error(`Unknown filter: ${filter}.`);
+    }
 };
 
-const mapStateToProps = (state) => {
-  return {
+const mapStateToProps = (state) => ({
     todos: getVisibleTodos(state.todos, state.visibilityFilter),
-  };
-};
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    onTodoClick: (id) => {
-      dispatch(toggleTodo(id));
+});
+
+const mapDispatchToProps = (dispatch) => ({
+
+    onTodoClick(id) {
+        dispatch(toggleTodo(id));
     },
-  };
-};
+});
 
 const VisibleTodoList = connect(
-  mapStateToProps,
-  mapDispatchToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(TodoList);
 
 export default VisibleTodoList;
