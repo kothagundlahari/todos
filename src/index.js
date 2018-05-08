ffb4a36dd79f748e649d8dd13d8f1d45b39fc12c
hari krisha <hari.k.krishna4@gmail.com>
2018-05-08 17:08:50 -0500

    added a persistance state

--- src/index.js
+++ src/index.js
@@ -5,12 +5,23 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import todoApp from './reducers';
 import App from './components/App';
+const persistedState = {
+  todos: [{
+    id: 0,
+    text: 'Welcome Back!',
+    completed: false
+  }]
+}
 
-const store = createStore(todoApp);
+const store = createStore(
+  todoApp,
+  persistedState
+)
+console.log(store.getState());
 
 render(
-  <Provider store={store}>
+  <Provider store = { store } >
     <App />
   </Provider>,
-  document.getElementById('root')
+    document.getElementById('root')
 );
