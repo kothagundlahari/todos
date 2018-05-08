bc1425aef97d617ecd4a6bafccab2ad28de23069
hari krisha <hari.k.krishna4@gmail.com>
2018-05-08 17:12:02 -0500

    changed Provider in the render function

--- src/index.js
+++ src/index.js
@@ -1,27 +1,36 @@
 import 'babel-polyfill';
 import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import {
+    render,
+} from 'react-dom';
+import {
+    Provider,
+} from 'react-redux';
+import {
+    createStore,
+} from 'redux';
 import todoApp from './reducers';
 import App from './components/App';
+
 const persistedState = {
-  todos: [{
-    id: 0,
-    text: 'Welcome Back!',
-    completed: false
-  }]
-}
+    todos: [{
+        id: 0,
+        text: 'Welcome Back!',
+        completed: false,
+    }],
+};
 
 const store = createStore(
-  todoApp,
-  persistedState
-)
+    todoApp,
+    persistedState
+);
 console.log(store.getState());
 
-render(
-  <Provider store = { store } >
-    <App />
-  </Provider>,
+render( <
+    Provider store = { store } >
+    <
+    App / >
+    <
+    /Provider>,
     document.getElementById('root')
 );
